8467e99e3085549fe3b71c1a9e3b59dfad0128e5
1119 pdh <1119pdh@gmail.com>
2021-11-17 17:11:33 +0900

    모델링 수정2

--- users/models.py
+++ users/models.py
@@ -3,7 +3,6 @@ from django.db import models
 class User(models.Model):
     kakao      = models.CharField(max_length=100)
     email      = models.CharField(max_length=100, unique=True)
-    contact    = models.CharField(max_length=15, unique=True)
     size       = models.PositiveIntegerField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
